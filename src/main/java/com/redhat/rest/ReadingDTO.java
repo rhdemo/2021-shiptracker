b4d9bc98dde310015a3b0cdba45f372e5a0be829
sebastienblanc <scm.blanc@gmail.com>
2021-02-05 12:31:34 +0100

    change to farenheit

--- src/main/java/com/redhat/rest/ReadingDTO.java
+++ src/main/java/com/redhat/rest/ReadingDTO.java
@@ -18,7 +18,7 @@ public class ReadingDTO {
     }
     
     public String getShippingId() {
-        return shippingId + " | " + getTemperature() + " Celcius";
+        return shippingId + " | " + getTemperature() + " Farenheit";
     }
 
     public Double getTemperature() {
