f886198abb24a61d496aedb95cc75fcddbd7024d
Jeffrey Phillips <jephilli@redhat.com>
2022-09-09 07:06:31 -0400

    Fix for unknown ports, show all regions

--- frontend/src/app/Port.tsx
+++ frontend/src/app/Port.tsx
@@ -3,18 +3,23 @@ import { Marker } from '@react-google-maps/api';
 import { PortType } from './types';
 import { getPortImage } from '../utilities/mapUtils';
 import ReadonlyIcon = google.maps.ReadonlyIcon;
+import DestinationPort from '../images/destination-target.svg';
+import OriginPort from '../images/origin-target.svg';
 
 interface MapPortProps {
   port: PortType;
+  isDestination?: boolean;
 }
 
-const Port: React.FC<MapPortProps> = ({ port }) => {
+const Port: React.FC<MapPortProps> = ({ port, isDestination }) => {
   const initializeMarker = (newMarker) => {
+    const portImage = getPortImage(port.name);
+    const url = portImage || (isDestination ? DestinationPort : OriginPort);
     const icon: ReadonlyIcon = {
-      url: getPortImage(port.name),
+      url,
       origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(29.33, 44),
-      scaledSize: new google.maps.Size(60, 60),
+      anchor: portImage ? new google.maps.Point(29.33, 44) : new google.maps.Point(10, 10),
+      scaledSize: portImage ? new google.maps.Size(60, 60) : new google.maps.Size(15, 15),
     };
     newMarker.setIcon(icon);
   };
